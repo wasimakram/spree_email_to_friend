263c7e8e14664c16c98470113596d868fceb524b
Wasim Akram <mailwasim@gmail.com>
2011-11-22 06:12:48 +0500

    updated keys

--- app/models/captcha_configuration.rb
+++ app/models/captcha_configuration.rb
@@ -2,8 +2,8 @@ class CaptchaConfiguration < Configuration
   THEMES = ['red', 'white', 'blackglass', 'clean', 'custom']
 
   # this keys works for localhost
-  preference :private_key, :string, :default => '6LfIsgEAAAAAAGfB6Z0lEUtdL3GVuBkRa9cYlMZz'
-  preference :public_key, :string, :default => '6LfIsgEAAAAAALpT20eiC3RslZQmmCbiNS-AUvSe'
-  preference :theme, :string, :default => 'red'
+  preference :private_key, :string, :default => '6LcfVMoSAAAAAFztTHP2_nQPrCl9tb9H_lliyI9v'
+  preference :public_key, :string, :default => '6LcfVMoSAAAAAPUOiPGmlDs-5qs1Y3QAMujOPELi'
+  preference :theme, :string, :default => 'clean'
   preference :use_captcha, :boolean, :default => true
 end
